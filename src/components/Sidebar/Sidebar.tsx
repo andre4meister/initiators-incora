50ae75054e60e6e13ef5f7ebd04751f36c49e613
kanedaSh0tar0 <1nikitos1003@gmail.com>
2022-10-20 21:00:14 +0300

    change const name

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -10,17 +10,17 @@ import styles from './Sidebar.module.scss';
 import { useTheme } from '../../hoc/ThemeProvider';
 
 const Sidebar: FC = () => {
-  const theme = useTheme();
+  const themeContext = useTheme();
   const navigate = useNavigate();
   const user = useAppSelector((state) => state.user.userData);
-  const [toggle, setToggle] = useState<boolean>(theme.theme !== 'light');
+  const [toggle, setToggle] = useState<boolean>(themeContext.theme !== 'light');
 
   const handleChangeTheme = () => {
     setToggle((prev) => !prev);
     if (toggle) {
-      theme.handleChangeTheme('light');
+      themeContext.handleChangeTheme('light');
     } else {
-      theme.handleChangeTheme('dark');
+      themeContext.handleChangeTheme('dark');
     }
   };
 
