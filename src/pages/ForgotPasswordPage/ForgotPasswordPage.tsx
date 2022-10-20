98f9fb1394c85c9407a9f055aa50013541107560
alexxandrrar <alexandroovnaa@gmail.com>
2022-10-20 22:49:59 +0300

    feat/ edit forgot password page

--- src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -1,5 +1,4 @@
 /* eslint-disable object-curly-newline */
-/* eslint-disable jsx-a11y/label-has-associated-control */
 import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import yupPattern from 'utils/yupPattern';
@@ -7,6 +6,7 @@ import Input from 'components/UI/Input/Input';
 import Button from 'components/UI/Button/Button';
 import { InitialGetAccessValues } from 'types/FormTypes';
 import { FC } from 'react';
+import { FrownOutlined } from '@ant-design/icons';
 import style from '../LoginPage/LoginPage.module.scss';
 
 const ForgotPasswordPage: FC = () => {
@@ -24,27 +24,34 @@ const ForgotPasswordPage: FC = () => {
 
   const { handleSubmit, handleChange, values, errors, touched } = formik;
   return (
-    <form className={style.forgot_page} onSubmit={handleSubmit}>
-      <h1 className={style.text}>Forgot your password?😞</h1>
-      <h5 className={style.forgot_page_text}>
-        Please enter your working email adress,where we can sent password reset informatin
-      </h5>
-      <div className={style.form_item}>
-        <label className={style.item} htmlFor="email" />
-        <Input
-          placeholder="Enter your email"
-          name="email"
-          classes="input"
-          type="email"
-          handleOnChange={handleChange}
-          value={values.email}
-        />
-        {touched.email && errors.email ? <div className={style.error}>{errors.email}</div> : null}
-      </div>
-      <Button classes="button-submit" handleOnClick={handleSubmit}>
-        Request reset link
-      </Button>
-    </form>
+    <div className={style.container}>
+      <form className={style.form} onSubmit={handleSubmit}>
+        <h1 className={style.text}>
+          Forgot your password
+          <FrownOutlined />
+        </h1>
+        <h5 className={style.forgot_page_text}>
+          Please enter your working email adress,where we can sent password
+          reset informatin
+        </h5>
+        <div className={style.forgot_page_item}>
+          <Input
+            placeholder="Enter your email"
+            name="email"
+            classes="input"
+            type="email"
+            handleOnChange={handleChange}
+            value={values.email}
+          />
+          {touched.email && errors.email ? (
+            <div className={style.error}>{errors.email}</div>
+          ) : null}
+        </div>
+        <Button classes="button-submit" handleOnClick={handleSubmit}>
+          Request reset link
+        </Button>
+      </form>
+    </div>
   );
 };
 
