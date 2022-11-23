096459ebffc95304a8bf63235a7d1c72a651b8d3
andre4meister <51925104+andre4meister@users.noreply.github.com>
2022-11-23 13:34:24 +0200

    used guestSelect in dashboard room

--- src/components/DashboardRoom/FullRoomInfo.tsx
+++ src/components/DashboardRoom/FullRoomInfo.tsx
@@ -1,4 +1,5 @@
 import { CalendarOutlined, ClockCircleOutlined } from '@ant-design/icons';
+import GuestsSelect from 'components/UI/GuestsSelect/GuestsSelect';
 import moment from 'moment';
 import { FC } from 'react';
 import { CommonMeetingType } from '../../types/CommonTypes';
@@ -16,16 +17,14 @@ const FullRoomInfo: FC<FullRoomInfoProps> = ({ booking }) => {
   )}-${booking.endTime.substring(0, 5)}`;
 
   return (
-    <div
-      className={styles.bookingContainer}
-    >
+    <div className={styles.bookingContainer}>
       <div className={styles.firstLine}>
         <div>
           <div className={styles.id}>{booking.title}</div>
         </div>
         <div className={styles.createdAt}>{editedCreatedAt}</div>
       </div>
-      <div>
+      <div className={styles.infoBookingBlock}>
         <CalendarOutlined className={styles.fullInfoIcon} />
         {`${moment.weekdays(booking.meetingDate).toString()}, ${moment(
           booking.meetingDate,
@@ -33,12 +32,12 @@ const FullRoomInfo: FC<FullRoomInfoProps> = ({ booking }) => {
           .startOf('month')
           .format('MMM')} ${moment(booking.meetingDate).get('date')}`}
       </div>
-      <div>
+      <div className={styles.infoBookingBlock}>
         <ClockCircleOutlined className={styles.fullInfoIcon} />
         {timeFromTo}
       </div>
       <div>
-        guests here
+        <GuestsSelect guests={booking.guests} />
       </div>
     </div>
   );
